a4cdbb5f7b9b0a1d97db7024ef50ac3d27282320
r-vernon <richard.vernon88@gmail.com>
2018-08-08 11:48:15 +0100

    execution stops sooner if not a click we're looking for reduced code redundancy

--- matSurf_fns/camControl/cam_bDownFcn.m
+++ matSurf_fns/camControl/cam_bDownFcn.m
@@ -1,9 +1,15 @@
 function cam_bDownFcn(src,event)
 % function called when patch, panel or axis button down callback triggered
 
-% get data
+% get camControl
 f_h = getFigHandle(src);
 camControl = getappdata(f_h,'camControl');
+
+% make sure click is 'normal' or 'extend'
+cState = strcmp(f_h.SelectionType(1),{'n','e'});
+if ~any(cState), return; end
+
+% get rest of data
 currVol = getappdata(f_h,'currVol');
 handles = getappdata(f_h,'handles');
 
@@ -11,55 +17,52 @@ handles = getappdata(f_h,'handles');
 camControl.mState = f_h.SelectionType;
 camControl.tStmp = clock;
 
+% get mouse pos. relative to panel
+% (currPnt - min(panelSz))/0.5*range(panelSz) - 1
+% if panel 0.25-1.0 and pnt 0.5, (0.5-0.25)/0.375 -1 = -1/3
+pSize = handles.axisPanel.Position;
+mPos = (f_h.CurrentPoint-pSize(1:2))./(0.5*pSize(3:4)) - 1;
+
+%--------------------------------------------------------------------------
 % set mouse movement function if relevant button press
 % (normal - LClick, alt - RClick, extend - ScrollClick/LRClick)
 % only using normal (rotate) and extend (pan) currently
 
-switch camControl.mState(1)
-    case 'n'  % normal, rotate (or possible click)
+if cState(1) % normal, rotate (or possible click)
+    
+    % work out where the callback came from
+    if strcmp(src.Tag,'brainPatch')
         
-        % work out where the callback came from
-        if strcmp(src.Tag,'brainPatch') 
+        % was a patch click, store intersection point
+        camControl.clPatch = true;
+        
+        % must *undo* current quat. rotation, use q' = r*q*inv(r)
+        % but actually want inv. rotmat, so doing inv(r)*q*r...
+        inv_qForm = [camControl.qForm(1),-camControl.qForm(2:4)];
+        ip = qMul(inv_qForm,qMul([0,event.IntersectionPoint],camControl.qForm));
+        camControl.ip = nearestNeighbor(currVol.TR,ip(2:4));
+        
+    else
+        % axis click
+        camControl.clPatch = false;
+    end
 
-            % was a patch click, store intersection point
-            camControl.clPatch = true;
-            
-            % must *undo* current quat. rotation, use q' = r*q*inv(r)
-            % but actually want inv. rotmat, so doing inv(r)*q*r...
-            inv_qForm = [camControl.qForm(1),-camControl.qForm(2:4)];
-            ip = qMul(inv_qForm,qMul([0,event.IntersectionPoint],camControl.qForm));
-            camControl.ip = nearestNeighbor(currVol.TR,ip(2:4));        
-            
-        else            
-            % axis click
-            camControl.clPatch = false;            
-        end
-        
-        % get current panel size
-        pSize = handles.axisPanel.Position;
-        
-        % get mouse pos on 'sphere' covering panel, radius 1
-        % (currPnt - min(panelSz))/0.5*range(panelSz) - 1
-        % if panel 0.25-1.0 and pnt 0.5, (0.5-0.25)/0.375 -1 = -1/3
-        camControl.mPos1 = zeros(1,3);
-        camControl.mPos1([1,3]) = (f_h.CurrentPoint-pSize(1:2))./(0.5*pSize(3:4)) - 1;
-        
-        % if mPos1 within circle (<=1), get depth from basic pythag., otherwise set to 0
-        currPos_SSq = sum(camControl.mPos1.^2);
-        if currPos_SSq <= 1, camControl.mPos1(2) = -sqrt(1-currPos_SSq); end
-
-        f_h.WindowButtonMotionFcn = @cam_mMoveFcn_rot;
-        
-    case 'e' % extend, pan
-        
-        % get mouse pos. relative to panel as above, just not bothering with depth
-        pSize = handles.axisPanel.Position;
-        camControl.mPos1 = (f_h.CurrentPoint-pSize(1:2))./(0.5*pSize(3:4)) - 1;
-        
-        f_h.WindowButtonMotionFcn = @cam_mMoveFcn_pan;
-        
-    otherwise % alt, do nothing
-        f_h.WindowButtonMotionFcn = '';
+    % get mouse pos on 'sphere' covering panel, radius 1
+    camControl.mPos1 = zeros(1,3);
+    camControl.mPos1([1,3]) = mPos;
+    
+    % if mPos1 within circle (<=1), get depth from basic pythag., otherwise set to 0
+    currPos_SSq = sum(camControl.mPos1.^2);
+    if currPos_SSq <= 1, camControl.mPos1(2) = -sqrt(1-currPos_SSq); end
+    
+    f_h.WindowButtonMotionFcn = @cam_mMoveFcn_rot;
+    
+else % extend, pan
+    
+    % set mouse position
+    camControl.mPos1 = mPos;
+    
+    f_h.WindowButtonMotionFcn = @cam_mMoveFcn_pan;
 end
 
 % set pointer ('fleur' is 4 arrows) and button up function
